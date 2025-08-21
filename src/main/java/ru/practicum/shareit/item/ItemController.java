968a73440a57eee1417282d422b07d87178a76b0
Yulia Kudlai <iu.5hi@yandex.ru>
2025-08-21 23:16:56 +0300

    sprint 14

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,53 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemRequestDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
+import ru.practicum.shareit.item.dto.ItemResponseUserDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.mapper.ItemMapper;
+import ru.practicum.shareit.item.service.ItemService;
+
+import java.util.List;
 
 /**
  * TODO Sprint add-controllers.
  */
 @RestController
 @RequestMapping("/items")
+@AllArgsConstructor
 public class ItemController {
+    private final ItemService service;
+    private final ItemMapper mapper;
+
+    @PostMapping
+    ItemResponseDto create( @Valid @RequestBody ItemRequestDto request, @RequestHeader("X-Sharer-User-Id") long ownerId) {
+        return mapper.toItemResponse(service.addItem(ownerId, mapper.toItem(request)));
+    }
+
+    @PatchMapping("{itemId}")
+    ItemResponseDto update(@RequestHeader("X-Sharer-User-Id") long ownerId,
+                           @Valid @RequestBody ItemUpdateDto request,
+                           @PathVariable long itemId) {
+        return mapper.toItemResponse(service.updateItem(itemId, ownerId, mapper.toItem(request)));
+
+    }
+
+    @GetMapping("{itemId}")
+    ItemResponseDto get(@PathVariable long itemId) {
+        return mapper.toItemResponse(service.getItem(itemId));
+
+    }
+
+    @GetMapping
+    List<ItemResponseUserDto> getUserItems(@RequestHeader("X-Sharer-User-Id") long ownerId) {
+        return mapper.toItemResponseList(service.getUserItems(ownerId));
+    }
+
+    @GetMapping("search")
+    List<ItemResponseUserDto> searchItems(@RequestParam  String text) {
+        return mapper.toItemResponseList(service.searchItems(text));
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,20 @@
+package ru.practicum.shareit.item.mapper;
+
+import org.mapstruct.Mapper;
+import ru.practicum.shareit.item.dto.ItemRequestDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
+import ru.practicum.shareit.item.dto.ItemResponseUserDto;
+import ru.practicum.shareit.item.dto.ItemUpdateDto;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+@Mapper(componentModel="spring")
+public interface ItemMapper {
+    Item toItem(ItemRequestDto request);
+    Item toItem(ItemUpdateDto update);
+    ItemResponseDto toItemResponse(Item item);
+    ItemResponseUserDto toItemResponseUser(Item item);
+    List<ItemResponseUserDto> toItemResponseList(List<Item> items);
+
+}
