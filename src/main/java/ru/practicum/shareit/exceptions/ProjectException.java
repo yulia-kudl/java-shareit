113678c14684970a0eb97360539ef82a21cde7b3
Yulia Kudlai <iu.5hi@yandex.ru>
2025-10-11 13:23:37 +0300

    sprint 16

--- src/main/java/ru/practicum/shareit/exceptions/ProjectException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class ProjectException extends RuntimeException {
-    public ProjectException(String message) {
-        super(message);
-    }
-}
