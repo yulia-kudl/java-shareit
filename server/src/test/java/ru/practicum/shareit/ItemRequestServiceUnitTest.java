1b049641fc2a79c0ab02ca7ec6c1f0514b546d71
Yulia Kudlai <iu.5hi@yandex.ru>
2025-10-12 18:14:03 +0300

    checkstyle

--- server/src/test/java/ru/practicum/shareit/ItemRequestServiceUnitTest.java
+++ server/src/test/java/ru/practicum/shareit/ItemRequestServiceUnitTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class ItemRequestServiceImplUnitTest {
+class ItemRequestServiceUnitTest {
 
     @Mock
     private RequestRepository requestRepository;
